34d83a7582034f464e6b1b9f9786e03a5bc246e4
PoPo <76515395+PoPoGH@users.noreply.github.com>
2020-12-23 18:14:56 +0100

    popo_shops v1.0

---
+++ popo_shops/fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'ESX Shops edit by PoPo'
+
+version '1.2'
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/es.lua',
+	'locales/sv.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'client/main.lua'
+}
+
+server_scripts {
+	'@es_extended/locale.lua',
+	'@mysql-async/lib/MySQL.lua',
+	'locales/de.lua',
+	'locales/br.lua',
+	'locales/en.lua',
+	'locales/fi.lua',
+	'locales/fr.lua',
+	'locales/es.lua',
+	'locales/sv.lua',
+	'locales/pl.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+dependency 'es_extended'
